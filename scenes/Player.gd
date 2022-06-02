f1d6026cb4eedd065516acbcd2d20dcb5be18e1d
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-06-02 22:52:09 +0300

    section 1 episode 7

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,23 +1,38 @@
 extends KinematicBody2D
 
-var gravity: = 300
+var gravity: = 1000
 var velocity: = Vector2.ZERO
-var maxHorizontalSpeed: = 100
-var jumpSpeed = 200
+var maxHorizontalSpeed: = 140
+var horizontalAcceleration: = 2000
+var jumpSpeed: = 360
+var jumpTerminationMultiplier = 4
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _process(delta: float) -> void:
-	var moveVector = Vector2.ZERO
-	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	var moveVector = get_movment_vector()
 	
-	velocity.x = moveVector.x * maxHorizontalSpeed
+	velocity.x += moveVector.x * horizontalAcceleration * delta
+	if moveVector.x == 0:
+		velocity.x = lerp(velocity.x, 0, .1)
+		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
+		
+	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
 	if moveVector.y < 0 && is_on_floor():
 		velocity.y = moveVector.y * jumpSpeed
+		
+	if velocity.y < 0 && !Input.is_action_pressed("jump"):
+		velocity.y += gravity * jumpTerminationMultiplier * delta
+	else:
+		velocity.y += gravity * delta
 
-	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)	
+
+func get_movment_vector() -> Vector2:
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return moveVector
