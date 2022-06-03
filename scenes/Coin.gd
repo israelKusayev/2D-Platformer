0bdc1a2a2b37dd6ee464d242872328c47dfc80e1
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-06-03 12:47:49 +0300

    section 2 episode 12

---
+++ scenes/Coin.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready() -> void:
+	$Area2D.connect("area_entered", self, "on_area_entered")
+	
+	
+func on_area_entered(area2d):
+	$AnimationPlayer.play("pickup")
+	call_deferred("disable_pickup")
+
+func disable_pickup():
+	$Area2D/CollisionShape2D.disabled = true
