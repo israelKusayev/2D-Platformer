d53cac36f9d1747affdba8c052b159c91d067342
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-06-09 09:22:08 +0300

    section 3 episode 24

--- scenes/BaseLevel.gd
+++ scenes/BaseLevel.gd
@@ -1,22 +1,36 @@
 extends Node2D
 
-var playerScene = preload("res://scenes/Player.tscn")
-var spwanPosition = Vector2.ZERO
-var currentPlayerNode: Player = null
+class_name BaseLevel
 
+signal coin_total_changed
+
+var playerScene = preload("res://scenes/Player.tscn")
+var spwanPosition := Vector2.ZERO
+var currentPlayerNode: Player = null
+var totalCoins := 0
+var collectedCoins := 0
 
 func _ready() -> void:
 	spwanPosition = ($Player as Player).global_position
 	register_player($Player as Player)
+	
+	coin_total_chaanged(get_tree().get_nodes_in_group("coin").size())
 
+func coin_collected():
+	collectedCoins += 1
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
 
+func coin_total_chaanged(newTotal):
+	totalCoins = newTotal
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
+	
 func register_player(player: Player):
 	currentPlayerNode = player
 	currentPlayerNode.connect("died", self, "on_player_died", [], CONNECT_DEFERRED)
 
 
 func create_player():
-	var playerInstance = playerScene.instance()
+	var playerInstance: Player = playerScene.instance()
 	add_child_below_node(currentPlayerNode, playerInstance)
 	playerInstance.global_position = spwanPosition
 	register_player(playerInstance)
