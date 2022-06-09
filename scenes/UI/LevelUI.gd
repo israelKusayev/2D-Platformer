6c39f3462f3e929eda3bed82d45d91788c1f1f2c
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-06-09 09:48:14 +0300

    section 3 episode 25

---
+++ scenes/UI/LevelUI.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	var baseLavels = get_tree().get_nodes_in_group("base_level")
+	
+	if baseLavels.size() > 0:
+		baseLavels[0].connect("coin_total_changed", self, "on_coin_total_changed")
+
+func on_coin_total_changed(total_coins: int, collectedCoins: int):
+	$MarginContainer/CoinLabel.text = str(collectedCoins, '/', total_coins)
