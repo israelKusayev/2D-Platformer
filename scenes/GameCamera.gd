7f13802576893b04b3802298da948b276a33dc20
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-06-12 22:10:26 +0300

    desction 4 episode 32

--- scenes/GameCamera.gd
+++ scenes/GameCamera.gd
@@ -5,7 +5,16 @@ class_name GameCamera
 var targetPosition = Vector2.ZERO
 
 export(Color, RGB) var backgroundColor
+export(OpenSimplexNoise) var shakeNoise
 
+var xNoiseSampleVector := Vector2.RIGHT
+var yNoiseSampleVector := Vector2.DOWN
+var xNoiseSamplePosition := Vector2.ZERO
+var yNoiseSamplePosition := Vector2.ZERO
+var noiseSampleTravelRate := 500
+var maxShakeOffset := 6
+var currentShakePrecentage := 0
+var shakeDecay := 4
 
 func _ready() -> void:
 	VisualServer.set_default_clear_color(backgroundColor)
@@ -15,7 +24,22 @@ func _process(delta: float) -> void:
 	acuire_target_position()
 
 	global_position = lerp(targetPosition, global_position, pow(2, -15 * delta))
+	
+	if currentShakePrecentage > 0:
+		xNoiseSamplePosition += xNoiseSampleVector * noiseSampleTravelRate * delta
+		yNoiseSamplePosition += yNoiseSampleVector * noiseSampleTravelRate * delta
+		var xSample = shakeNoise.get_noise_2d(xNoiseSamplePosition.x, xNoiseSamplePosition.y)
+		var ySample = shakeNoise.get_noise_2d(yNoiseSamplePosition.x, yNoiseSamplePosition.y)
+		
+		var calculatedOffset = Vector2(xSample, ySample) * maxShakeOffset * pow(currentShakePrecentage, 2)
+		offset = calculatedOffset
+		
+		currentShakePrecentage = clamp(currentShakePrecentage - shakeDecay, 0, 1)
+		
 
+
+func apply_shake(precentage: int):
+	currentShakePrecentage = clamp(currentShakePrecentage + precentage, 0, 1)
 
 func acuire_target_position():
 	var players := get_tree().get_nodes_in_group("player")
