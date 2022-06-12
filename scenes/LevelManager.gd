1df97f10a6bf35e87d3ece7bac021b19a4f68bc2
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-06-12 21:45:10 +0300

    desction 3 episode 31

--- scenes/LevelManager.gd
+++ scenes/LevelManager.gd
@@ -4,8 +4,7 @@ export(Array, PackedScene) var levelScenes
 
 var currentLevelIndex := 0
 
-func _ready() -> void:
-	change_level(currentLevelIndex)
+
 	
 func change_level(levelIndex):
 	currentLevelIndex = levelIndex
---
+++ scenes/Main.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _ready() -> void:
+	$"/root/LevelManager".change_level(0)
